e3be7587380eabe1601f12304f32f4ffae061148
Antuhrium <antyriyman@gmail.com>
2024-08-04 18:37:05 +0300

    changes: mobile slider

--- src/components/RiskCard/RiskCard.tsx
+++ src/components/RiskCard/RiskCard.tsx
@@ -1,6 +1,7 @@
 import "./RiskCard.css"
 import { RISK_CARD } from "../../constant.ts"
 import { RiskCardsType } from "../../constant.ts"
+import IconRect from "../IconRect/IconRect.tsx";
 
 interface RiskCardsProps {
   variant: RiskCardsType;
@@ -9,7 +10,8 @@ interface RiskCardsProps {
 const RiskCard:React.FC<RiskCardsProps> = ({ variant }) => {
   return (
     <div className="riskcard">
-      <img className="riskcard-icon" src={RISK_CARD[variant].icon} />
+      <IconRect icon={RISK_CARD[variant].icon} />
+      {/* <img className="riskcard-icon" src={RISK_CARD[variant].icon} /> */}
       <h3 className="riskcard-title btn-text-blue">{RISK_CARD[variant].text}</h3>
       <h4 className="riskcard-desc">{RISK_CARD[variant].description}</h4>
     </div>
